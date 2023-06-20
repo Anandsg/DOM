b0bc3b6f47499d26123e9764a4b3e546a2043d98
Anandagadugin <gaduginanand984@gmail.com>
2023-06-20 17:58:33 +0530

    reviced get element by class name

--- dom.js
+++ dom.js
@@ -1,16 +1,42 @@
 // EXAMINE THE DOM OBJECST
 
-console.log(document.title);
-console.log(document.URL);
-console.log(document.domain);
+// console.log(document.title);
+// console.log(document.URL);
+// console.log(document.domain);
 
-document.title = 123;
-console.log(document.doctype);
-console.log(document.all[10]);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.all[10]);
 
-document.all[13].textContent = "Hello";
+// document.all[13].textContent = "Hello";
 
-console.log(document.links);
-console.log(document.images);
+// console.log(document.links);
+// console.log(document.images);
 
+// GET ELEMENT BY ID
 
+// console.log(document.getElementById("header-title"));
+
+// HeaderTitle = document.getElementById("header-title");
+// HeaderTitle = document.getElementById("main-header");
+
+// console.log(HeaderTitle);
+// HeaderTitle.textContent = "Hello";
+// HeaderTitle.innerHTML = "<h2>Hello2</h2>";
+
+// HeaderTitle.style.borderBottom = "solid 2px #000";
+
+// GET ELEMENT BY CLASS NAME
+
+var Items = document.getElementsByClassName("list-group-item");
+console.log(Items);
+console.log(Items[2]);
+Items[2].style.fontWeight = "bold";
+
+// changing one item background color
+Items[3].style.backgroundColor = "skyblue";
+
+// changing all the items bckground color
+for(var i=0;i<Items.length;i++){
+    Items[i].style.backgroundColor = 'skyblue';
+}
