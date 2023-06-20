071688b40c23efc7f37e8f3a7b05346015c6080a
Anandagadugin <gaduginanand984@gmail.com>
2023-06-20 17:05:12 +0530

    Examine the DOM objects

---
+++ dom.js
@@ -0,0 +1,16 @@
+// EXAMINE THE DOM OBJECST
+
+console.log(document.title);
+console.log(document.URL);
+console.log(document.domain);
+
+document.title = 123;
+console.log(document.doctype);
+console.log(document.all[10]);
+
+document.all[13].textContent = "Hello";
+
+console.log(document.links);
+console.log(document.images);
+
+
